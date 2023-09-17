b9dcf806a38f6fe25b1c16cf7318824de9fae783
peanutbuttersharkface <peanutbuttersharkface@gmail.com>
2023-09-16 18:56:53 -0600

    changed getContacts to getDatabse

--- controller/contacts.js
+++ controller/contacts.js
@@ -3,7 +3,7 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getContacts().db().collection('Contacts').find();
+    const result = await mongodb.getDatabase().db().collection('Contacts').find();
     result.toArray().then((Contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(Contacts);
